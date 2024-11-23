880b88db0399248479d921cedb80ca468362ba61
twright <tom.wright.dev@gmail.com>
2024-11-23 15:49:16 +1100

    [scribe] Fix eror in postgres import, add logger to postgres service

--- scribe/src/services/database/postgres.ts
+++ scribe/src/services/database/postgres.ts
@@ -1,10 +1,15 @@
-import { Client as PostgresClient, QueryConfig } from "pg";
+// Cannot import individual postgres modules due to:
+// https://github.com/brianc/node-postgres/issues/2819
+import pg from "pg";
+import { Logger } from "../logger";
+import util from "util";
 
 /**
  * Service for handling connection and queries to Postgres database.
  */
 export class PostgresDatabaseService {
-  protected postgresClient: PostgresClient;
+  protected postgresClient: pg.Client;
+  protected logger: Logger;
 
   constructor(params: {
     userName: string;
@@ -12,8 +17,11 @@ export class PostgresDatabaseService {
     host: string;
     port: number;
     database: string;
+    logger: Logger;
   }) {
-    this.postgresClient = new PostgresClient({
+    this.logger = params.logger;
+
+    this.postgresClient = new pg.Client({
       user: params.userName,
       password: params.password,
       host: params.host,
@@ -25,13 +33,23 @@ export class PostgresDatabaseService {
   /**
    * Wrapper around database client to enforce the use of prepared statements to access the database.
    */
-  public async query(preparedStatement: QueryConfig): Promise<void> {
-    // Establish connection to the server.
-    this.postgresClient.connect();
+  public async query(preparedStatement: pg.QueryConfig): Promise<void> {
+    try {
+      await this.postgresClient.connect();
 
-    await this.postgresClient.query(preparedStatement);
+      const result = await this.postgresClient.query(preparedStatement);
 
-    // Disconnect from server.
-    this.postgresClient.end();
+      this.logger.debug({
+        message: "postgres query result",
+        data: JSON.stringify(result),
+      });
+
+      await this.postgresClient.end();
+    } catch (error) {
+      this.logger.error({
+        message: "Failed to write data to RDS",
+        data: util.format(error),
+      });
+    }
   }
 }
